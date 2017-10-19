5c3aea1d6c5138b1bf50ae26612c94e192c17266
Travis Payne <travis.payne84@me.com>
2017-10-19 15:31:06 -0400

    Remove unused code.

--- FormstackSharp/model/Form.cs
+++ FormstackSharp/model/Form.cs
@@ -158,29 +158,4 @@ namespace FormstackSharp.model
 		public string Html{ get; set; }
 
 	}
-
-	public partial class GettingStarted
-	{
-		public static GettingStarted FromJson(string json)
-		{
-			return JsonConvert.DeserializeObject<GettingStarted>(json, Converter.Settings);
-		}
-	}
-
-	public static class Serialize
-	{
-		public static string ToJson(this GettingStarted self)
-		{
-			return JsonConvert.SerializeObject(self, Converter.Settings);
-		}
-	}
-
-	public class Converter
-	{
-		public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-		{
-			MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-			DateParseHandling = DateParseHandling.None,
-		};
-	}
 }
